5d0ecc3b940630f61959b8b7af6d3329c0d7d2ff
zymsn <38268193+zymsn@users.noreply.github.com>
2019-11-08 17:21:21 +0800

    Update redis_del_key.py

--- redis/redis_del_key.py
+++ redis/redis_del_key.py
@@ -1,8 +1,8 @@
 import redis
-redis_port = ['172.31.0.251','172.31.0.252']
+redis_port = ['','']
 # redis_port = ['7000','7001','7002']
-# redis_delete = ["TMS:INFRASTRUCTURE:PROPERTIES:*"]
-redis_delete = ["TMS:DEPOT:DATA:LIST"]
+# redis_delete = [""]
+redis_delete = [""]
 for i in redis_port:
     r = redis.StrictRedis(host=i, port='6379', db=0)
     # r = redis.StrictRedis(host='10.155.90.164', port=i, db=0)
